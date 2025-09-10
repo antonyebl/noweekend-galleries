9bc1593be47467c7673d06e50fe2a07edf7a072b
antonyebl <antonyebl@gmail.com>
2025-09-10 17:30:34 +0200

    Update generate-json.mjs

--- generate-json.mjs
+++ generate-json.mjs
@@ -8,15 +8,15 @@ const __dirname = path.dirname(__filename);
 const rootDir = path.resolve(__dirname, '..');
 
 async function processGalleries() {
-  const galleryDirs = await fs.readdir(__dirname);
+  const rootContents = await fs.readdir(rootDir);
   
-  for (const dir of galleryDirs) {
+  for (const dir of rootContents) {
     if (dir.startsWith('gallery_')) {
-      const galleryPath = path.join(__dirname, dir);
+      const galleryPath = path.join(rootDir, dir);
       console.log(`Processing gallery: ${galleryPath}`);
 
       const files = (await fs.readdir(galleryPath))
-        .filter(file => file.toLowerCase().endsWith('.jpg'))
+        .filter(file => file.toLowerCase().endsWith('.jpg') || file.toLowerCase().endsWith('.jpeg'))
         .sort();
 
       const imageData = await Promise.all(files.map(async file => {
